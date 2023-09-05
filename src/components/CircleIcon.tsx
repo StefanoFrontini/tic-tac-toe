660b320cc9b2bb6a8ac1d1d08a8a6ed59dce24c3
Stefano <stefanofrontini75@gmail.com>
2023-09-05 18:31:04 +0200

    adds animation to icons

--- src/components/CircleIcon.tsx
+++ src/components/CircleIcon.tsx
@@ -1,9 +1,18 @@
 import styles from "./CircleIcon.module.scss";
+import { motion } from "framer-motion";
 interface CircleIconProps {
   small?: boolean;
 }
 function CircleIcon({ small = false }: CircleIconProps): JSX.Element {
-  return <div className={`${styles.circle} ${small && styles.small}`}></div>;
+  return (
+    <motion.div
+      initial={{ scale: 0 }}
+      animate={{ scale: 1 }}
+      exit={{ scale: 0 }}
+      className={`${styles.circle} ${small && styles.small}`}
+      key="circle"
+    ></motion.div>
+  );
 }
 
 export default CircleIcon;
--- src/ui/Board/Cell/Cell.tsx
+++ src/ui/Board/Cell/Cell.tsx
@@ -3,6 +3,7 @@ import CrossIcon from "../../../components/CrossIcon";
 import styles from "./Cell.module.scss";
 import type { Player, CellState, PlayerScore } from "../lib/ai";
 import { result, player, isWinningState } from "../lib/ai";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface CellProps {
   icon: string;
@@ -22,14 +23,19 @@ function Cell({
   updateScore,
   playerScore,
 }: CellProps): JSX.Element {
-  const empty = icon === "empty" && <div className={styles.empty}></div>;
+  const empty = icon === "empty" && (
+    <div key="empty" className={styles.empty}></div>
+  );
   const X = icon === "X" && <CrossIcon />;
   const Y = icon === "O" && <CircleIcon />;
 
   function handleClick(): void {
     if (gameState[i] !== "empty") return;
+
     if (player(gameState) === "O") return;
+
     const nextState = result(gameState, i);
+
     updateGameState(() => {
       if (isWinningState(nextState)) {
         const winningPlayer = player(nextState) === "X" ? "O" : "X";
@@ -37,18 +43,25 @@ function Cell({
       }
       return nextState;
     });
-    // updateGameState(nextMove);
     if (!isWinningState(nextState)) {
       setTimeout(() => computerMove(nextState), 1000);
     }
   }
 
   return (
-    <div className={styles.cell} onClick={handleClick}>
-      {empty}
-      {X}
-      {Y}
-    </div>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+      className={styles.cell}
+      onClick={handleClick}
+    >
+      <AnimatePresence>
+        {empty}
+        {X}
+        {Y}
+      </AnimatePresence>
+    </motion.div>
   );
 }
 
